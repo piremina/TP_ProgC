468b14d5ad960a647def016763c4a0a3f24e4fd8
piremina <premi94240@gmail.com>
2025-04-16 15:05:43 +0200

    Update cercle.c

--- TP1/src/cercle.c
+++ TP1/src/cercle.c
@@ -1,20 +1,22 @@
+// cercle.c
 #include <stdio.h>
-#include <math.h>
+#include <math.h>  // Pour M_PI
 
 int main() {
-    double rayon; // Utilisation de double pour une meilleure précision
+    double rayon;
+    double aire, perimetre;
 
     // Demander à l'utilisateur d'entrer le rayon
     printf("Entrez le rayon du cercle : ");
-    scanf("%lf", &rayon); // Utilisation de %lf pour lire un double
+    scanf("%lf", &rayon);
 
     // Calcul de l'aire et du périmètre
-    double aire = M_PI * rayon * rayon;      // Utilisation de M_PI pour la valeur de pi
-    double perimetre = 2 * M_PI * rayon;
+    aire = M_PI * rayon * rayon;
+    perimetre = 2 * M_PI * rayon;
 
     // Affichage des résultats
-    printf("L'aire du cercle est : %.2lf\n", aire); // Utilisation de %.2lf pour afficher un double avec 2 décimales
-    printf("Le périmètre du cercle est : %.2lf\n", perimetre);
+    printf("Aire du cercle : %.2f\n", aire);
+    printf("Périmètre du cercle : %.2f\n", perimetre);
 
     return 0;
 }
