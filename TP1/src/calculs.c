b01c5cb210d1b05db4e3c72c51736db635f53870
piremina <premi94240@gmail.com>
2025-04-16 17:18:06 +0200

    Update calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,47 @@
+#include <stdio.h>
 
+int main() {
+    int num1=5;
+    int num2=4;   
+    char op='+';          
+
+    switch (op) {
+        case '+':
+            printf("%d + %d = %d\n", num1, num2, num1 + num2);
+            break;
+        case '-':
+            printf("%d - %d = %d\n", num1, num2, num1 - num2);
+            break;
+        case '*':
+            printf("%d * %d = %d\n", num1, num2, num1 * num2);
+            break;
+        case '/':
+            if (num2 != 0) {
+                printf("%d / %d = %d\n", num1, num2, num1 / num2);
+            } else {
+                printf("Erreur : Division par zéro impossible.\n");
+            }
+            break;
+        case '%':
+            if (num2 != 0) {
+                printf("%d %% %d = %d\n", num1, num2, num1 % num2);
+            } else {
+                printf("Erreur : Division par zéro impossible.\n");
+            }
+            break;
+        case '&':
+            printf("%d & %d = %d\n", num1, num2, num1 & num2);
+            break;
+        case '|':
+            printf("%d | %d = %d\n", num1, num2, num1 | num2);
+            break;
+        case '~':
+            printf("~%d = %d\n", num1, ~num1);  // ~ effectue une opération de complément à un
+            break;
+        default:
+            printf("Opérateur invalide.\n");
+            break;
+    }
+
+    return 0;
+}
