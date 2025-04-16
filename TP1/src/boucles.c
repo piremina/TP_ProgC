26b6c9546b9373ffd73360f063937659442a7eb2
piremina <premi94240@gmail.com>
2025-04-16 16:39:19 +0200

    Update boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -3,11 +3,6 @@
 int main() {
     int compteur = 5; 
 
-    if (compteur >= 10) {
-        printf("Le compteur doit être inférieur à 10.\n");
-        return 1;
-    }
-
     for (int i = 1; i <= compteur; i++) {
         for (int j = 1; j <= i; j++) {
             if (i >= 3 && i != compteur && j != 1 && j != i)
