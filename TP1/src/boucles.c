0e8dc05b59d69212543f327dc42ae29340a3b64c
piremina <premi94240@gmail.com>
2025-04-16 16:33:59 +0200

    Update boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -10,13 +10,4 @@ int main() {
 
     for (int i = 1; i <= compteur; i++) {
         for (int j = 1; j <= i; j++) {
-            if (i % 2 == 0 && j != 1 && j != i)
-                printf("# ");
-            else
-                printf("* ");
-        }
-        printf("\n");
-    }
-
-    return 0;
-}
+            if (i % 2 == 0
