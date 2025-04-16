2e89f93cd112c75741942d14b3621657622048f8
piremina <premi94240@gmail.com>
2025-04-16 17:42:31 +0200

    Update binaire.c

--- TP1/src/binaire.c
+++ TP1/src/binaire.c
@@ -5,7 +5,7 @@ void afficherBinaire(int nombre) {
     int bitTrouve = 0;
 
     for (i = 31; i >= 0; i--) {
-        int bit = (nombre >> i) & 1; 
+        int bit = (nombre >> i) & 1;  
         
         if (bit == 1 || bitTrouve || i == 0) {
             bitTrouve = 1;
@@ -16,4 +16,12 @@ void afficherBinaire(int nombre) {
 }
 
 int main() {
-    int valeurs[] = {0, 4096, 65536, 65535, 1024
+    int valeur=0;
+
+    for (int i = 0; i < taille; i++) {
+        printf("Décimal : %d\tBinaire : ", valeurs[i]);
+        afficherBinaire(valeurs[i]);
+    }
+
+    return 0;
+}
