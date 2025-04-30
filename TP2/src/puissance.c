4385ef426aa6d7798ca8b4ff301f71942ce0ce2e
piremina <premi94240@gmail.com>
2025-04-30 14:38:38 +0200

    Update puissance.c

---
+++ TP2/src/puissance.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a = 2;  
+    int b = 3;  
+    int resultat = 1;
+
+    // Calcul de a^b en multipliant a par lui-même b fois
+    for (int i = 0; i < b; i++) {
+        resultat *= a;
+    }
+
+    printf("%d élevé à la puissance %d est : %d\n", a, b, resultat);
+    return 0;
+}
