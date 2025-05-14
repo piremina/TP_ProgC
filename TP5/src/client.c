ad9eeddcf0e7c7f73ddd2200b5fe46b651ea1504
piremina <premi94240@gmail.com>
2025-05-14 19:50:09 +0200

    Update client.c

--- TP5/src/client.c
+++ TP5/src/client.c
@@ -82,7 +82,7 @@ int main()
   memset(&server_addr, 0, sizeof(server_addr));
   server_addr.sin_family = AF_INET;
   server_addr.sin_port = htons(PORT);
-  server_addr.sin_addr.s_addr = INADDR_ANY;
+  server_addr.sin_addr.s_addr = 10.0.63.4;
 
   // demande de connection au serveur
   int connect_status = connect(socketfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
